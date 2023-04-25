3e8c7a4cd65f16a956fdd74faa3797044c20d9b7
romanlusnikov <aectann28@gmail.com>
2023-04-25 13:13:20 +0400

    finalTask

--- Task1/Line.java
+++ Task1/Line.java
@@ -5,6 +5,7 @@ public class Line {
     private double pointY1;
     private double pointХ2;
     private double pointY2;
+    private double length;
 
     public Line(double pointХ1, double pointY1, double pointХ2, double pointY2) {
         this.pointХ1 = pointХ1;
@@ -14,6 +15,16 @@ public class Line {
     }
 
     public double Length(){
-        return (pointХ2-pointХ1)+(pointY2-pointY1);
+        return this.length= (pointХ2-pointХ1)+(pointY2-pointY1);
+
+    }
+
+    public void Check(Line line){
+        this.length= (pointХ2-pointХ1)+(pointY2-pointY1);
+        if (this.length==line.length){
+
+            System.out.println("Длина равна");
+        } else
+            System.out.println("Длина не равна");
     }
 }
--- Task1/Main.java
+++ Task1/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
        Line line1 = new Line(1,1,2,2);
        Line line2 = new Line(-3,0,1,1);
-        System.out.println(line1.Length()+" " + line2.Length());
+       line1.Check(line2);
+       line2.Check(line2);
+
+      System.out.println(line1.Length()+" " + line2.Length());
 
 
     }
