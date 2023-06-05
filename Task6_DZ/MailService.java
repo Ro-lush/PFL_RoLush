c760aebd18ea0b787db45d6cf7cc48a0d6374ac1
romanlusnikov <aectann28@gmail.com>
2023-06-05 16:01:56 +0400

    finalTask6 - final

--- Task6_DZ/MailService.java
+++ Task6_DZ/MailService.java
@@ -5,30 +5,32 @@ import java.util.*;
 import java.util.function.Consumer;
 
 public class MailService<T> implements Consumer<NamePer<T>> {
-
-
-    Map<String, List<T>> tListMap = new HashMap<>();
-
-
+    Map<String, List<T>> tListMap = new Map<>();
     public Map<String, List<T>> getMailBox() {
-       return tListMap;
+        return tListMap;
     }
-
 
     @Override
     public void accept(NamePer<T> tNamePer) {
-        List<T> tList = new ArrayList<>();
-        tList.add(tNamePer.getContent());
-        if (tListMap.containsKey(tNamePer.getTo())){
-           String str = tListMap.get(tNamePer.getTo()).toString().replace("[","")
-                   .replace("]", "\"")+","+"\""+tNamePer.getContent();
+        if (tListMap.containsKey(tNamePer.getTo())) {
 
-           List<T> doubleList = new ArrayList<>();
-           doubleList.add((T) str);
-            tListMap.put(tNamePer.getTo(), doubleList);
+            List<T> tList = tListMap.get(tNamePer.getTo());
+            tList.add(tNamePer.getContent());
+            tListMap.put(tNamePer.getTo(), tList);
+
 
         } else {
-            tListMap.put(tNamePer.getTo(),tList);
+            List<T> tList = new ArrayList<>();
+            tList.add(tNamePer.getContent());
+            tListMap.put(tNamePer.getTo(), tList);
+        }
+    }
+
+    private static class Map<M, T> extends HashMap<M, T> {
+        public T get(Object key) {
+            T temp = super.get(key);
+            if (temp == null) temp = (T) Collections.emptyList();
+            return temp;
         }
 
     }
